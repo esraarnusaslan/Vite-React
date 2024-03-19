583faeadc5dd128dba1f304425481e83dfb9bf4e
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-19 22:31:06 +0300

    "Vite-React"

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Login from './Login';
+
+function App() {
+    //javascipt codes are here
+
+    return (
+        //html codes are here
+        <div>
+            <Login />
+        </div>
+    );
+}
+
+export default App;
