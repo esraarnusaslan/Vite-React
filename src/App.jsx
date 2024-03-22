2510bca9e269fff61a2d3028a4def643a1ff5037
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-22 14:29:57 +0300

    "usestate"

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import './App.css';
 import Hello from './Hello';
 import Login from './Login';
@@ -7,20 +8,34 @@ import Product from './components/Product';
 function App() {
     //javascipt codes are here
 
-    const productName = 'shirt'
+    //const productName = 'shirt';
+
+    const [firstName, setFirstName] = useState('esra');
+    const [names, setNames] = useState(['esra', 'volkan', 'tom', 'jerry']);
+    const [userInfo, setUserInfo] = useState({ username: 'esra, userId:1' });
+
     return (
         //html codes are here
         <div>
-           {/*  <Login />
+            {/*  <Login />
             <Hello /> 
 
             <Product productName="shoes" productPrice="100"/>
             <Product productName="hat" productPrice="300"/>
-            <Product productName={productName} productPrice="100"/>*/}
+            <Product productName={productName} productPrice="100"/>
 
             <Container>
             <Product />
             </Container>
+            */}
+            {firstName}
+            <button onClick={() => setFirstName('esra arnus aslan')}>
+                Click
+            </button>
+            {names.map((name, index) => (
+                <div key={index}>{name}</div>
+            ))}
+            {userInfo.username} {userInfo.userId}
         </div>
     );
 }
