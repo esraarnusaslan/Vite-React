388205f805194dbd227c17c41628386f762cfa46
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-26 14:16:14 +0300

    "course component added"

--- my-courses-project/src/App.jsx
+++ my-courses-project/src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css';
-import Header from './Header';
+import Course from './components/Course';
+import Header from './components/Header';
+import { courses } from './Data';
+import './css/Course.css';
 
 function App() {
     return (
         <div>
             <Header />
+            <div className="course-main">
+                {courses?.map((course) => (
+                    <Course key={course?.id} course={course} />
+                ))}
+            </div>
         </div>
     );
 }
---
+++ my-courses-project/src/Data.jsx
@@ -0,0 +1,38 @@
+import Image1 from './images/image-1.jpg';
+import Image2 from './images/image-2.jpg';
+import Image3 from './images/image-3.jpg';
+import Image4 from './images/image-4.jpg';
+export const courses = [
+    {
+        id: 1,
+        title: 'React - The Complete Guide',
+        description: 'Learn React - The Complete Guide',
+        price: 100,
+        link: 'https://www.udemy.com/course/react-the-complete-guide-incl-redux/',
+        image: Image1,
+    },
+    {
+        id: 2,
+        title: 'The Complete 2022 Web Development Bootcamp',
+        description: 'Learn Web Development',
+        price: 200,
+        link: 'https://www.udemy.com/course/the-complete-web-development-bootcamp/',
+        image: Image2,
+    },
+    {
+        id: 3,
+        title: 'The Complete JavaScript Course 2022: From Zero to Expert!',
+        description: 'Learn JavaScript',
+        price: 300,
+        link: 'https://www.udemy.com/course/the-complete-javascript-course/',
+        image: Image3,
+    },
+    {
+        id: 4,
+        title: 'Learn Restful WEB API, JavaScript and HTML: Web Services',
+        description: 'Learn Web Development',
+        price: 400,
+        link: 'https://www.udemy.com/course/learn-html-javascript-restful-api-from-scratch-web-services/learn/lecture/32502610?start=0#overview',
+        image: Image4,
+    },
+];
