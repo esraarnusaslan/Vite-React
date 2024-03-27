865580559c7eac9ea17c51ec95d4b7a58e462345
Esra Arnus Aslan <esra.arnus@gmail.com>
2024-03-27 13:58:27 +0300

    "axios get put delete "

--- API-USING/src/App.jsx
+++ API-USING/src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect } from 'react';
 const base_URL = 'http://localhost:3005';
 
 function App() {
-    const getAllUsers = async () => {
+    /*   const getAllUsers = async () => {
         const response = await axios.get(`${base_URL}/users`);
         console.log(response.data);
     };
@@ -15,12 +15,30 @@ function App() {
         console.log(response.data);
     };
 
+       const createUser =  async(newUser) => {
+        const response=await axios.post(`${base_URL}/users`, newUser);
+        console.log(response.data);
+    }  */
+
+    /*   const updateUser = async(userId,updatedUser) => {
+        const response=await axios.put(`${base_URL}/users/${userId}`, updatedUser);
+        console.log(response.data);
+    } */
+
+    /* const deleteUserById = async (userId) => {
+        const deletedResponse = await axios.delete(
+            `${base_URL}/users/${userId}`
+        );
+        console.log(deletedResponse.data);
+    }; */
+
     useEffect(() => {
         //getAllUsers();
-        getUserById(1);
+        //getUserById(1);
+        //createUser({username:"esra",password:"esra123"})
+        //updateUser(1,{username:"esraarnus123",password:"esraarnus123"})
+        //deleteUserById('4ae4');
     }, []);
-
- 
 
     return <div></div>;
 }
